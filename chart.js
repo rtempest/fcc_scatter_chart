91b9ab88c0756a24bc05044bf0bc888eed1a6f6b
Rosie Tempest <rosietempest@gmail.com>
2021-01-06 21:57:21 -0800

    created title text

--- chart.js
+++ chart.js
@@ -1,4 +1,4 @@
-url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
+const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
 
 d3.json(url, function (error, json) {
     console.log(json)
@@ -12,6 +12,13 @@ d3.json(url, function (error, json) {
         .append('svg')
         .attr('height', h)
         .attr('width', w)
+
+    let title = svg.append('text')
+        .attr('x', w / 2)
+        .attr('y', 30)
+        .attr('text-anchor', 'middle')
+        .attr('id', 'title')
+        .text('Doping Allegations in Professional Cycling')
 
 
 
